ea2440637c8bfb05b087ac0aa271683a3fb01bdf
mikecip10 <mikecip10@outlook.com>
2021-06-25 13:22:34 -0400

    yup

--- VacationPy/config.py
+++ VacationPy/config.py
@@ -1,1 +1,2 @@
 api_key = "05c562a0eea8ca8dc3cd57af3c89bd92"
+gmap_key = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyDCHUjJpH2H_vP83IKMb3zhxBTiJgCothQ"
